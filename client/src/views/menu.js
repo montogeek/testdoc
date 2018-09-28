3378b351fd0c9727d424d203898057ff59c26854
Fernando Montoya <montogeek@gmail.com>
2018-09-28 18:11:46 +0200

    BROKEN, trying to add totals

--- client/src/views/menu.js
+++ client/src/views/menu.js
@@ -2,6 +2,8 @@ import React, { Component } from "react"
 import { connect } from "react-redux"
 import { Table } from "antd"
 
+console.log(Table)
+
 class OthersList extends Component {
   constructor() {
     super()
@@ -105,7 +107,70 @@ class FoodList extends Component {
     return (
       <div>
         <h1>{name}</h1>
-        <Table dataSource={items} loading={loading} columns={this.columns} />
+        <Table
+          components={{
+            body: {
+              wrapper: props => {
+                return (
+                  <tbody {...props}>
+                    <React.Fragment>
+                      {props.children}
+                      <tr className="ant-table-row">
+                        <td class="">
+                          <span class="ant-table-row-indent indent-level-0" />
+                          Little Bacon Cheeseburger
+                        </td>
+                        <td class="">193</td>
+                        <td class="">0.2</td>
+                        <td class="">0</td>
+                        <td class="">13.200000000000001</td>
+                        <td class="">14.62121212121212</td>
+                        <td class="">2.924242424242424</td>
+                        <td class="">0</td>
+                        <td class="">Alice noticed, had powdered.</td>
+                      </tr>
+                    </React.Fragment>
+                  </tbody>
+                )
+              }
+            }
+          }}
+          dataSource={items}
+          loading={loading}
+          columns={this.columns}
+          footer={data => {
+            return "footer"
+            // return (
+            // <div className="flex justify-around">
+            //   <div>Total</div>
+            //   <div>{data.reduce((a, b) => a + b.cost, 0)}</div>
+            //   <div>{data.reduce((a, b) => a + b.shareKid, 0)}</div>
+            //   <div>{data.reduce((a, b) => a + b.shareAdult, 0)}</div>
+            //   <div>{data.reduce((a, b) => a + b.totalshare, 0)}</div>
+            //   <div>{data.reduce((a, b) => a + b.costShare, 0)}</div>
+            //   <div>{data.reduce((a, b) => a + b.costShare * b.shareKid, 0)}</div>
+            //   <div>{data.reduce((a, b) => a + b.costShare * b.shareAdult, 0)}</div>
+            //   <div className="w-10" />
+            // </div>
+            // <tbody className="ant-table-tbody">
+            //   <tr class="ant-table-row">
+            //     <td class="">
+            //       <span class="ant-table-row-indent indent-level-0" />
+            //       Little Bacon Cheeseburger
+            //     </td>
+            //     <td class="">193</td>
+            //     <td class="">0.2</td>
+            //     <td class="">0</td>
+            //     <td class="">13.200000000000001</td>
+            //     <td class="">14.62121212121212</td>
+            //     <td class="">2.924242424242424</td>
+            //     <td class="">0</td>
+            //     <td class="">Alice noticed, had powdered.</td>
+            //   </tr>
+            // </tbody>
+            // )
+          }}
+        />
       </div>
     )
   }
@@ -113,7 +178,7 @@ class FoodList extends Component {
 
 class Menu extends Component {
   render() {
-    const { event, loading, kids, adults } = this.props
+    const { event, loading } = this.props
 
     if (!event) return "loading"
 
--- code/database/factories/ItemFactory.php
+++ code/database/factories/ItemFactory.php
@@ -8,8 +8,8 @@ $factory->define(App\Models\Item::class, function (Faker $faker) {
     return [
         'name' => rand(0,1) ? $faker->foodName() : $faker->beverageName(),
         'cost' => random_int(50, 200),
-        'shareKid' => mt_rand(0, 5) / 10,
-        'shareAdult' => mt_rand(0, 10) / 10,
+        'shareKid' => mt_rand(0, 5),
+        'shareAdult' => mt_rand(0, 10),
         'bought' => (bool)random_int(0, 1),
         'notes' => $faker->realText(30),
         'event_id' => random_int(1, 20),
