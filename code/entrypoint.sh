6e05361ebb8408f9ce9a1fdf865292b7cd7ac541
Fernando Montoya <montogeek@gmail.com>
2019-02-10 15:13:35 +0100

    Optimize Laravel in production

--- code/entrypoint.sh
+++ code/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
-
 php artisan migrate --force
 php artisan db:seed --force
 php artisan passport:keys
 php artisan passport:client --password --name="Frontend"
+
+php artisan config:cache
+php artisan route:cache
 
 php-fpm
