594180be8d213f46b86e292332d5b20e9c79c73a
Fernando Montoya <montogeek@gmail.com>
2019-02-10 15:24:02 +0100

    Dont register Telescope in production

--- code/app/Providers/AppServiceProvider.php
+++ code/app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Telescope\TelescopeServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if ($this->app->isLocal()) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
         }
     }
