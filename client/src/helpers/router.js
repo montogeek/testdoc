6a1edeba4936e4a312acab1958f7cb13ae53376a
Fernando Montoya <montogeek@gmail.com>
2019-02-19 01:23:22 +0100

    Fix file name

--- client/src/helpers/router.js
+++ client/src/helpers/router.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Redirect } from "react-router-dom"
 import { connect } from "react-redux"
-import Layout from "../components/Layout";
+import Layout from "../components/PrivateLayout"
 
 const RedirectRoute = props => {
   const { Component, user, ...rest } = props
