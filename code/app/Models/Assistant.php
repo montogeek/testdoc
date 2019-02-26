1ef3c7d276ba917ddf8a8e88fb78ace0c120efa5
Fernando Montoya <montogeek@gmail.com>
2019-02-27 00:41:14 +0100

    Redo Assistants table

--- code/app/Models/Assistant.php
+++ code/app/Models/Assistant.php
@@ -10,4 +10,10 @@ class Assistant extends Model
     use SoftDeletes;
     
     protected $dates = ['deleted_at'];
+    protected $appends = ['total'];
+
+    public function getTotalAttribute()
+    {
+        return $this->attributes['total'] = $this->adults + $this->kids;
+    }
 }
