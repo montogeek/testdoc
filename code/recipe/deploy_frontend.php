d6cf3eeb052d023d0700bba90c5c23826c65bf8a
Fernando Montoya <montogeek@gmail.com>
2019-07-28 00:14:11 +0200

    GitLab: Fixed client direactory

--- code/recipe/deploy_frontend.php
+++ code/recipe/deploy_frontend.php
@@ -6,8 +6,8 @@ after('hook:ready', 'deploy_client');
 
 desc('Deploy client');
 task('deploy_client', function () {
-  $path = run('pwd && ls -alh');
-  run("echo $path");
+  // $path = run('pwd && ls -alh');
+  // run("echo $path");
   upload('../client/build/', '{{release_path}}/frontend');
   writeln("Client build uploaded");
 });
