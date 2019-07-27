441ae8f9ed9cb363a94fa6ea38d80ffa79468342
Fernando Montoya <montogeek@gmail.com>
2019-07-28 00:08:59 +0200

    GitLab: Added log do deploy client

--- code/recipe/deploy_frontend.php
+++ code/recipe/deploy_frontend.php
@@ -6,6 +6,8 @@ after('hook:ready', 'deploy_client');
 
 desc('Deploy client');
 task('deploy_client', function () {
-  upload('client/build/', '{{release_path}}/frontend');
+  $path = run('pwd && ls -alh');
+  run("echo $path");
+  upload('../client/build/', '{{release_path}}/frontend');
   writeln("Client build uploaded");
 });
