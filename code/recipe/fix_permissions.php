376f170c091dd7d3ac6447269ba820625a30a1f1
Fernando Montoya <montogeek@gmail.com>
2019-07-28 01:05:43 +0200

    GitLab: Change hook for fix permissions

--- code/recipe/fix_permissions.php
+++ code/recipe/fix_permissions.php
@@ -2,12 +2,12 @@
 
 namespace Deployer;
 
-after('hook:ready', 'fix_permissions');
+after('hook:done', 'fix_permissions');
 
 desc('Fix permissions');
 task('fix_permissions', function () {
   within('{{release_path}}', function () {
-    run('sudo chmod -R 777 bootstrap/cache/*');
+    run('sudo chmod -R 777 bootstrap/cache/');
     run('sudo chmod -R 777 storage/*');
     writeln("Permission fixed");
   });
